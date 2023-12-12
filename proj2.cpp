7d47cd7fb5f4f223ab99a54127ca493780d00239
lohztmz <92582049+lohztmz@users.noreply.github.com>
2023-12-12 00:29:17 +0000

    need new dfs method(with colors)

--- proj2.cpp
+++ proj2.cpp
@@ -8,55 +8,56 @@ class GraphMatrix {
 public:
     int nodes;
     std::vector<std::vector<int>> graph;
-    void Graph(int mnodes){
-        nodes=mnodes;
-        graph.resize(nodes+1);
-        for(int i=0;i<=nodes;i++)
-            graph[i].resize(nodes+1);
-    }
+    GraphMatrix(int mnodes) : nodes(mnodes), graph(nodes + 1, std::vector<int>(nodes + 1, 0)) {}
+
 
     void addEdge(int i, int f){
         if(graph[i][f]==0)
             graph[i][f]=1; 
     }
 
-    void itdfs(int node, std::vector<bool> visited, std::vector<int> order) {
+    void itdfs(int node, std::vector<int>& color, std::list<int>& order) {
         std::stack<int> queue;
-        visited[node] = true;
-
         queue.push(node);
+
         while(!queue.empty()){
             int current = queue.top();
             queue.pop();
 
-            if(!visited[current]){
+            if(color[current]==1){
                 visited[current] = true;
-                order.insert(order.begin(),current);
+                order.push_front(current);
+               
                 for (int neighbour = 1; neighbour <= nodes; ++neighbour) {
-                    if (graph[current][neighbour] && !visited[neighbour]) {
+                    
+                    if (graph[current][neighbour]!=0 && !visited[neighbour]) {
                         queue.push(neighbour);
+    
                     }
                 }
             }
         }
     }
 
-    void dfs(std::vector<int> order){
+    void dfs(std::list<int>& order){
         std::vector<bool> visited(nodes+1,false);
         for(int i=1;i<=nodes;i++){
             if(!visited[i]){
                 itdfs(i,visited,order);
             }
         }
+        for(int i:order){
+            printf("%d ",i);
+        }
+        
     }
 
     void transpose() {
-        std::vector<int> nodeline (nodes+1);
+        std::vector<int> nodeline (nodes+1,0);
         std::vector<std::vector<int>> transposed(nodes+1,nodeline);
         for (int i = 1; i <= nodes; i++) {
-            for (int j = i+1; j <= nodes; j++) {
+            for (int j = 1; j <= nodes; j++) {
                 transposed[i][j] = graph[j][i];
-                transposed[j][i] = graph[i][j];
             }
         }
         graph = transposed;
@@ -65,40 +66,42 @@ public:
 
 
     GraphMatrix getSCCGraph(){
-        std::vector<std::vector<int>> components;
+        std::vector<std::list<int>> components;
         int ncomponents=0;
-        std::vector<bool> visited;
-        std::vector<int> order;
+
+        std::vector<bool> visited(nodes+1,false);
+        std::list<int> order;
+
         dfs(order);
+        for(int i :order){
+            printf("%d",i);
+        }
         transpose();
+
         while(!order.empty()){
-            
             int node = *order.begin();
-            order.erase(order.begin());
+            order.pop_front();
 
             if(!visited[node]){
-                std::vector<int> currSCC;
-                std::vector<int> comp;
+                std::list<int> currSCC;
                 ncomponents++;
                 itdfs(node,visited,currSCC);
                 components.push_back(currSCC);
             }
         }
 
-        GraphMatrix SCCgraph;
-        SCCgraph.Graph(ncomponents+1);
+        GraphMatrix SCCgraph(ncomponents);
+        
         for(int i=0; i< ncomponents; i++){
-            while(!components[i].empty()){
-                int current = *components[i].begin();
-                components[i].erase(components[i].begin());
-                for(int child: graph[current]){
+            std::list<int> curSCC= components[i];
+            for(std::list<int>::iterator it= curSCC.begin();it!=curSCC.end();it++){
+                for(int child: graph[*it]){
                     for (int j = 0; j < ncomponents; ++j) {
-                        std::vector<int> visitingSCC = components[j];
-                        while(!visitingSCC.empty()){
-                            if (std::find(visitingSCC.begin(), visitingSCC.end(), child) != visitingSCC.end()) { 
-                                SCCgraph.addEdge(j,i); 
-                                break;
-                            }
+                        std::list<int> visitingSCC = components[j];
+                        if (std::find(visitingSCC.begin(), visitingSCC.end(), child) != visitingSCC.end()) { 
+                            SCCgraph.addEdge(j,i); 
+                            break;
+                        
                         }
                     }
                 }
@@ -113,13 +116,21 @@ int main(){
     int numusers,nrelations;
     int user1, user2;
     scanf("%d %d", &numusers, &nrelations);
-    GraphMatrix tugaNet;
-    tugaNet.Graph(numusers);
+    GraphMatrix tugaNet(numusers);
     while(nrelations!=0){
         scanf("%d %d", &user1, &user2);
         tugaNet.addEdge(user1,user2);
         nrelations--;
     }
+   
+    
     tugaNet=tugaNet.getSCCGraph();
+    for (int i = 1; i <= tugaNet.nodes; i++) {
+            for (int j = 1; j <= tugaNet.nodes; j++) {
+                printf("%d ", tugaNet.graph[i][j]);
+            }
+            printf("\n");
+        }
     return 0;
+    
 }
