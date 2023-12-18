a9530b67346706664f4d1ccda3516d9ef1e8ca90
lohztmz <92582049+lohztmz@users.noreply.github.com>
2023-12-18 10:42:30 +0000

    Update proj2.cpp

--- proj2.cpp
+++ proj2.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <list>
 #include <stack>
-#include <algorithm>
 
 class GraphList {
 public:
@@ -11,23 +10,23 @@ public:
     std::vector<std::list<int>> transposedGraph;
     GraphList(int mnodes) : nodes(mnodes), graph(nodes + 1), transposedGraph(nodes+1) {}
 
-
+    //adiciona a adjacencia noo grafo normal e no transposto
     void addEdge(int i, int f){
         graph[i].push_front(f);
         transposedGraph[f].push_front(i);
     }
 
-    void itdfs(int node, std::vector<int>& colour, std::list<int>& order) {
+    void dfsVisit(int node, std::vector<int>& colour, std::list<int>& order) {
         std::stack<int> queue;
-
+        std::list<int>::iterator neighbour;
         queue.push(node);
-
+        int current;
         while(!queue.empty()){
-            int current = queue.top();
+            current = queue.top();
 
             if(colour[current]==0){
                 colour[current] = 1;
-                for (std::list<int>::iterator neighbour=graph[current].begin();neighbour!=graph[current].end();neighbour++) {
+                for (neighbour=graph[current].begin();neighbour!=graph[current].end();neighbour++) {
                     if (colour[*neighbour]!=2) {
                         queue.push(*neighbour);
                     }
@@ -47,25 +46,53 @@ public:
         std::vector<int> colour(nodes+1,0);
         for(int i=1;i<=nodes;i++){
             if(colour[i]==0){
-                itdfs(i,colour,order);
+                dfsVisit(i,colour,order);
             }
-        }
-        for(int i:order){
-            printf("%d ",i);
-        }
-        
+        }        
     }
 
-
-
+    int second_dfs(int node, std::vector<int>& parents, std::vector<int>& max_jump_node){
+        std::stack<int> queue;
+        queue.push(node);
+        int parent= node;
+        int value =0;
+        std::list<int>::iterator neighbour;
+        while(!queue.empty()){
+            node = queue.top();
+            queue.pop();
+            parents[node]=parent;
+            for (neighbour=transposedGraph[node].begin();neighbour!=transposedGraph[node].end();neighbour++) {
+                if(parents[*neighbour]!=parent){
+                    if(parents[*neighbour]!=0){
+                        if (max_jump_node[parents[*neighbour]]>=value)
+                            value = max_jump_node[parents[*neighbour]]+1;
+                    } else {
+                        queue.push(*neighbour);
+                    }
+                }
+                
+            }
+        }
+        max_jump_node[parent]=value;
+        return value;
+    }
 
     int getMaxJumps(){
-        std::vector<int> parent(nodes+1,0);
+        std::vector<int> parents(nodes+1,0);
         std::vector<int> max_jump_node(nodes+1,0);
         std::list<int> order;
         int max_jump=0;
+        int node,i;
+        int max_current_SCC;
         dfs(order);
-        
+        while((node= *order.begin())!=0){
+            order.pop_front();
+            if(parents[node]!=0){
+                continue;
+            }else if((max_current_SCC=second_dfs(node,parents,max_jump_node))>max_jump){
+                max_jump= max_current_SCC;
+            }
+        }
         return max_jump;
     }
 };
@@ -75,6 +102,7 @@ int main(){
     int user1, user2;
     scanf("%d %d", &numusers, &nrelations);
     GraphList tugaNet(numusers);
+
     while(nrelations!=0){
         scanf("%d %d", &user1, &user2);
         tugaNet.addEdge(user1,user2);
